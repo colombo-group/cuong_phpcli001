a91504ae91da44e37a2e7b98a0879266431465b1
le cuong <lecuong732@gmail.com>
2017-07-05 11:44:28 +0700

    edit file over.php and remove vendor folder

--- over.php
+++ over.php
@@ -1,22 +1,19 @@
 <?php
     require_once('vendor/autoload.php');
     $climate = new League\CLImate\CLImate;
-   $climate->arguments->add([
-    'num' => [
-        'longPrefix'  => 'num'
-    ]
-]);
-$climate->arguments->parse();
-$num = $climate->arguments->get('num');
+    $climate->arguments->add([
+        'num' => [
+            'longPrefix'  => 'num'
+        ]
+    ]);
+    $climate->arguments->parse();
+    $num = $climate->arguments->get('num');
     $defaults = [39, 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97];
     if($num) {
         for($i = 1; $i <= $num; $i ++) {
             $str = '';
-            for($j = 1; $j <= $num; $j ++) {
-                if($j <= $i) {
-                    $str .= $j;
-                }
-
+            for($j = 1; $j <= $i; $j ++) {
+                $str .= $j;
             }
             $climate->style->addColor('rage', $defaults[array_rand($defaults, 1)]);
             $climate->rage($str);
@@ -28,11 +25,8 @@ $num = $climate->arguments->get('num');
         $input->accept(function($response) {
             for($i = 1; $i <= $response; $i ++) {
                 $str = '';
-                for($j = 1; $j <= $response; $j ++) {
-                    if($j <= $i) {
-                        $str .= $j;
-                    }
-
+                for($j = 1; $j <= $i; $j ++) {
+                    $str .= $j;
                 }
                 $GLOBALS['climate']->style->addColor('rage', $GLOBALS['defaults'][array_rand($GLOBALS['defaults'], 1)]);
                 $GLOBALS['climate']->rage($str);
